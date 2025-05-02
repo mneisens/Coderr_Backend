cf1066e68d8b49f9cc115403fbc64b830c2030f3
michael <contact@michael-neisens.de>
2025-05-02 13:13:57 +0200

    add apps in setting.py

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_app',
+    'baseinfo_app',
+    'offers_app',
+    'orders_app',
+    'profiles_app',
+    'reviews_app',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auth_app.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
