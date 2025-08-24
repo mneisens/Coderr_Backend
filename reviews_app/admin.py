011444c3a8bda961de0c5e1dd4499137b0803b02
michael <contact@michael-neisens.de>
2025-08-24 20:22:30 +0200

    create extra permission.py

--- reviews_app/admin.py
+++ reviews_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'reviewer', 'business_user', 'rating', 'created_at', 'updated_at']
+    list_filter = ['rating', 'created_at', 'updated_at']
+    search_fields = ['reviewer__username', 'business_user__username', 'description']
+    readonly_fields = ['created_at', 'updated_at']
+    ordering = ['-updated_at']
