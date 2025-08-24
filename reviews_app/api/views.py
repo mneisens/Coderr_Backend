0009c000c6dd17cf18ea12fb3fa6b50998ab4795
michael <contact@michael-neisens.de>
2025-08-24 19:45:40 +0200

    create Reviews views + url

---
+++ reviews_app/api/views.py
@@ -0,0 +1,41 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from django.db.models import Q
+from ..models import Review
+from .serializers import ReviewSerializer, ReviewCreateSerializer, ReviewUpdateSerializer
+from .permissions import IsCustomerUser, IsReviewOwner
+from rest_framework.exceptions import ValidationError
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['business_user', 'reviewer']
+    ordering_fields = ['updated_at', 'rating']
+    ordering = ['-updated_at']
+
+    def get_queryset(self):
+        return Review.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ReviewCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return ReviewUpdateSerializer
+        return ReviewSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated(), IsCustomerUser()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAuthenticated(), IsReviewOwner()]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        business_user = serializer.validated_data['business_user']
+        if Review.objects.filter(reviewer=self.request.user, business_user=business_user).exists():
+            raise ValidationError('Sie haben bereits eine Bewertung für diesen Business User abgegeben.')
+        serializer.save(reviewer=self.request.user)
