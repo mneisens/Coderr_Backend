b69e430a8c1631fa2c1f4490b1e99ce09fd97295
michael <contact@michael-neisens.de>
2025-08-24 17:22:46 +0200

    create models for reviews

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from auth_app.models import CustomUser
 
-# Create your models here.
+class Review(models.Model):
+    business_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews_received',limit_choices_to={'type': 'business'}
+    )
+    reviewer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reviews_given')
+    rating = models.IntegerField(choices=[
+        (1, '1 Stern'),
+        (2, '2 Sterne'),
+        (3, '3 Sterne'),
+        (4, '4 Sterne'),
+        (5, '5 Sterne'),
+    ])
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['business_user', 'reviewer']
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return f"Review von {self.reviewer.username} für {self.business_user.username} - {self.rating} Sterne"
